4e392cffbd9fb064fe94579d6134545f4f847aab
Henri Hulski <henri.hulski@gazeta.pl>
2016-12-09 23:03:41 +0100

    Remove sql_debug

--- morepath_ponyorm/run.py
+++ morepath_ponyorm/run.py
@@ -1,17 +1,12 @@
 import morepath
-from pony.orm import sql_debug
 
 from .app import App
 from .model import db
 
 
 def run():   # pragma: no cover
-    db.bind('sqlite', 'user.db', create_db=True)
+    db.bind('sqlite', 'morepath_ponyorm.db', create_db=True)
     db.generate_mapping(create_tables=True)
-
-    # This shows the SQL query in console
-    # Remove in production
-    sql_debug(True)
 
     morepath.autoscan()
     morepath.run(App())
