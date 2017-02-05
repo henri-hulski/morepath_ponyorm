eebbf7cc3ec2bb6daeb9dd7122dcfa94d01fd6d3
Henri Hulski <henri.hulski@gazeta.pl>
2017-02-05 14:44:02 +0100

    Identify order of view and database hooks

--- morepath_ponyorm/model.py
+++ morepath_ponyorm/model.py
@@ -1,3 +1,5 @@
+import logging
+
 from pony.orm import Database, PrimaryKey, Optional
 
 db = Database()
@@ -13,3 +15,9 @@ class Document(db.Entity):
     id = PrimaryKey(int, auto=True)
     title = Optional(str)
     content = Optional(str)
+
+    def before_insert(self):
+        logging.warning('Here is the before_insert hook of the model.')
+
+    def after_insert(self):
+        logging.warning('Here is the after_insert hook of the model.')
